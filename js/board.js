1f7529a2d425a41301294c6df4f309cf15fa46c1
Hannah Hu <hannahlhu@gmail.com>
2014-12-18 14:48:57 -0800

    Filling out the JS a little bit. Skeleton code at this point.

--- js/board.js
+++ js/board.js
@@ -26,12 +26,12 @@ function Group() {
 
 	this.digits = new Array();
 
-	this.addDigit = function() {
-
+	this.addDigit = function(digit) {
+		group.digits.push(digit);
 	};
 
-	this.removeDigit = function() {
-
+	this.removeDigit = function(digit) {
+		group.digits = removeFromArray(group.digits, digit);
 	};
 
 };
@@ -51,11 +51,43 @@ function Block() {
 	Group.call(this);
 }
 
+function Board() {
+	var board = this;
+
+	this.rows = this.makeRows();
+	this.columns = this.makeColumns();
+	this.blocks = this.makeBlocks();
+	this.cells = this.makeCells();
+	this.filledCellCount = 0;
+
+	this.makeRows = function() {
+
+	};
+
+	this.makeColumns = function() {
+
+	};
+
+	this.makeBlocks = function() {
+
+	};
+
+	this.makeCells = function() {
+
+	};
+
+}
+
 function yell() {
 	alert("Hey!");
 }
 
-function union_arrays (x, y) {
+
+function initBoard(boardData) {
+
+}
+
+function union_arrays(x, y) {
 	var obj = {};
 	for (var i = x.length-1; i >= 0; -- i)
 		 obj[x[i]] = x[i];
@@ -69,5 +101,20 @@ function union_arrays (x, y) {
 	return res;
 }
 
-var BOARD = "[0,8,2,4,0,5,7,0,3,0,1,0,6,8,3,5,0,0,5,3,9,7,0,0,6,4,8,0,7,4,3,5,6,1,8,9,8,0,0,9,0,7,0,0,6,9,6,3,1,2,8,4,7,0,7,4,5,0,0,9,2,3,1,0,0,8,5,3,4,0,6,0,3,0,6,2,0,1,8,5,0]"
-var SOLUTION = "[6,8,2,4,9,5,7,1,3,4,1,7,6,8,3,5,9,2,5,3,9,7,1,2,6,4,8,2,7,4,3,5,6,1,8,9,8,5,1,9,4,7,3,2,6,9,6,3,1,2,8,4,7,5,7,4,5,8,6,9,2,3,1,1,2,8,5,3,4,9,6,7,3,9,6,2,7,1,8,5,4]"
+function removeFromArray(arr, item) {
+	return $.grep(arr, function(value) {
+		return value != item;
+	});
+};
+
+var BOARD = [0,8,2,4,0,5,7,0,3,0,1,0,6,8,3,5,0,0,5,3,9,7,0,0,6,4,8,0,7,4,3,5,6,1,8,9,8,0,0,9,0,7,0,0,6,9,6,3,1,2,8,4,7,0,7,4,5,0,0,9,2,3,1,0,0,8,5,3,4,0,6,0,3,0,6,2,0,1,8,5,0]
+var SOLUTION = [6,8,2,4,9,5,7,1,3,4,1,7,6,8,3,5,9,2,5,3,9,7,1,2,6,4,8,2,7,4,3,5,6,1,8,9,8,5,1,9,4,7,3,2,6,9,6,3,1,2,8,4,7,5,7,4,5,8,6,9,2,3,1,1,2,8,5,3,4,9,6,7,3,9,6,2,7,1,8,5,4]
+
+var tmpBoard = []
+for (var i = 0; i < 9; i++) {
+	tmpBoard[i] = [];
+	for (var j = 0; j < 9; j++) {
+		tmpBoard[i][j] = BOARD[9 * i + j];
+	}
+}
+
