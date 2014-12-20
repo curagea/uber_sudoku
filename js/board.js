dfeca9c07bc0f2757a90f8b0c142bb67eccfaf06
Hannah Hu <hannahlhu@gmail.com>
2014-12-19 22:23:18 -0800

    Ripping out a lot. Turns out having 3 different arrays for storing essentially the same data is a pain to maintain :/. Now only one array is holding the digits, and helper functions now "create" the appropriate row/column/block, rather than fetching said row/column/block from some 2D array.

--- js/board.js
+++ js/board.js
@@ -1,27 +1,15 @@
 var BOARD = [0,8,2,4,0,5,7,0,3,0,1,0,6,8,3,5,0,0,5,3,9,7,0,0,6,4,8,0,7,4,3,5,6,0,8,9,8,0,0,9,0,7,0,0,6,9,6,3,0,0,8,4,7,0,7,4,5,0,0,0,2,3,1,0,0,8,5,3,4,0,6,0,3,0,6,2,0,1,8,5,0]
 var SOLUTION = [6,8,2,4,9,5,7,1,3,4,1,7,6,8,3,5,9,2,5,3,9,7,1,2,6,4,8,2,7,4,3,5,6,1,8,9,8,5,1,9,4,7,3,2,6,9,6,3,1,2,8,4,7,5,7,4,5,8,6,9,2,3,1,1,2,8,5,3,4,9,6,7,3,9,6,2,7,1,8,5,4]
 
+var SOLVED = 'solved!';
+var INVALID = 'booooo';
 
-function Cell(row, col, digit, input) {
-	var cell = this;
-
-	this.row = row;
-	this.col = col;
-	this.block = [blockCoordinate(row), blockCoordinate(col)];
-	this.digit = digit;
-	this.input = input;
-
-	this.input.value = (digit !== 0) ? digit : '';
-	this.input.disabled = (digit !== 0);
-};
 
 function Board(boardContainer) {
 	var board = this;
 
-	this.rows = [];
-	this.cols = [];
-	this.blocks = [];
-	this.cells = [];
+	this.grid = [];
+
 	this.boardContainer = boardContainer;
 	this.inputs = boardContainer.find('input');
 	this.filledCellCount = 0;
@@ -42,18 +30,12 @@ function Board(boardContainer) {
 	};
 
 	this.initBoard = function(boardData) {
-		board.makeStorageGrid(this.rows);
-		board.makeStorageGrid(this.cols);
-		board.makeBlocksGrid(this.blocks);
-		board.makeStorageGrid(this.cells);
+		board.grid = boardData;
 
 		for (var row = 0; row < 9; row++) {
 			for (var col = 0; col < 9; col++) {
 				var digit = boardData[9 * row + col];
 				board.initCell(digit, board.inputs[9 * row + col]);
-				board.rows[row][col] = digit;
-				board.cols[col][row] = digit;
-				board.blocks[blockCoordinate(row)][blockCoordinate(col)][blockCellLocation(row, col)] = digit;
 				if (digit !== 0) {
 					board.upFillCount();
 				}
@@ -81,15 +63,13 @@ function Board(boardContainer) {
 	};
 
 	this.insertDigit = function(row, col, digit) {
-		board.rows[row][col] = digit;
-		board.cols[col][row] = digit;
-		board.blocks[blockCoordinate(row)][blockCoordinate(col)][blockCellLocation(row, col)] = digit;
+		board.grid[9 * row + col] = digit;
+		board.upFillCount();
 	};
 
 	this.removeDigit = function(row, col) {
-		board.rows[row][col] = 0;
-		board.cols[col][row] = 0;
-		blocks[blockCoordinate(row)][blockCoordinate(col)][blockCellLocation(row, col)] = 0;
+		board.grid[9 * row + col] = digit;
+		board.downFillCount();
 	};
 
 	this.checkInput = function(inputElement) {
@@ -102,42 +82,66 @@ function Board(boardContainer) {
 		} else {
 			value = parseInt(inputElement.val());
 
-			var unionArray = union_arrays(board.rows[row], board.cols[col]);
-			unionArray = union_arrays(unionArray, board.blocks[blockCoordinate(row)][blockCoordinate(col)]);
+			var unionArray = [];
+			var cellRow = board.fetchRowDigits(row);
+			var cellCol = board.fetchColumnDigits(col);
+			var cellBlock = board.fetchBlockDigits(row, col);
+			$.merge(unionArray, cellRow);
+			$.merge(unionArray, cellCol);
+			$.merge(unionArray, cellBlock);
+
 			if (!isValidDigit(value) || ($.inArray(value, unionArray) !== -1)) {
-				board.yell(inputElement);
+				board.showBadCell(inputElement);
 			} else {
 				board.insertDigit(row, col, value);
 				inputElement.removeClass('invalid');
+				if (board.filledCellCount === 81) {
+					board.gameFinished(SOLVED);
+				}
 			}
 		}
 	};
 
-	this.yell = function(inputElement) {
+	this.gameFinished = function(endState) {
+
+	};
+
+	this.showBadCell = function(inputElement) {
 		inputElement.addClass('invalid');
 	};
 
-}
+	this.fetchRowDigits = function(row) {
+		var rowDigits = [];
+		for (var i = 0; i < 9; i++) {
+			rowDigits.push(board.grid[9 * row + i]);
+		}
+		return rowDigits;
+	};
 
-function union_arrays(x, y) {
-	var obj = {};
-	for (var i = x.length-1; i >= 0; -- i)
-		 obj[x[i]] = x[i];
-	for (var i = y.length-1; i >= 0; -- i)
-		 obj[y[i]] = y[i];
-	var res = []
-	for (var k in obj) {
-		if (obj.hasOwnProperty(k))
-			res.push(obj[k]);
-	}
-	return res;
-}
+	this.fetchColumnDigits = function(col) {
+		var colDigits = [];
+		for (var i = 0; i < 9; i++) {
+			colDigits.push(board.grid[9 * i + col]);
+		}
+		return colDigits;
+	};
 
-function blockCoordinate(x) {
-	return Math.floor(x/3);
-}
-function blockCellLocation(x, y) {
-	return 3 * (x % 3) + (y % 3);
+	this.fetchBlockDigits = function(row, col) {
+		var blockDigits = [];
+		var blockRow = Math.floor(row/3);
+		var blockCol = Math.floor(col/3);
+
+		for (var i = 0; i < 3; i++) {
+			for (var j = 0; j < 3; j++) {
+				var absoluteRow = 3 * blockRow + i;
+				var absoluteCol = 3 * blockCol + j;
+				blockDigits.push(board.grid[9 * absoluteRow + absoluteCol])
+			}
+		}
+
+		return blockDigits;
+	};
+
 }
 
 function isValidDigit(value) {
