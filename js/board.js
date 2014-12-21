e0462bdb391094cb54c25aebd424ffcb2e8291b8
Hannah Hu <hannahlhu@gmail.com>
2014-12-20 21:05:38 -0800

    Removed invalid cell count. Originally I was going to use it for showing bad end-states, but then I figured that's a lot of trouble to deal with. Also took out some helper functions due to redundancy, and fixed how removeDigit() works so it does not mess up validCellCount.

--- js/board.js
+++ js/board.js
@@ -1,10 +1,6 @@
 var BOARD = [0,8,2,4,0,5,7,0,3,0,1,0,6,8,3,5,0,0,5,3,9,7,0,0,6,4,8,0,7,4,3,5,6,0,8,9,8,0,0,9,0,7,0,0,6,9,6,3,0,0,8,4,7,0,7,4,5,0,0,0,2,3,1,0,0,8,5,3,4,0,6,0,3,0,6,2,0,1,8,5,0]
 var SOLUTION = [6,8,2,4,9,5,7,1,3,4,1,7,6,8,3,5,9,2,5,3,9,7,1,2,6,4,8,2,7,4,3,5,6,1,8,9,8,5,1,9,4,7,3,2,6,9,6,3,1,2,8,4,7,5,7,4,5,8,6,9,2,3,1,1,2,8,5,3,4,9,6,7,3,9,6,2,7,1,8,5,4]
 
-var SOLVED = 'solved!';
-var INVALID = 'booooo';
-
-
 function Board(boardContainer) {
 	var board = this;
 
@@ -13,7 +9,6 @@ function Board(boardContainer) {
 	this.boardContainer = boardContainer;
 	this.inputs = boardContainer.find('input');
 	this.validCellCount = 0;
-	this.invalidCellCount = 0;
 
 	this.initBoard = function(boardData) {
 		board.grid = boardData;
@@ -23,7 +18,7 @@ function Board(boardContainer) {
 				var digit = boardData[9 * row + col];
 				board.initCell(digit, board.inputs[9 * row + col]);
 				if (digit !== 0) {
-					board.upFillCount();
+					board.validCellCount++;
 				}
 			}
 		}
@@ -40,22 +35,16 @@ function Board(boardContainer) {
 		}
 	}
 
-	this.upFillCount = function() {
-		board.validCellCount = board.validCellCount++;
-	};
-
-	this.downFillCount = function() {
-		board.validCellCount = board.validCellCount--;
-	};
-
 	this.insertDigit = function(row, col, digit) {
 		board.grid[9 * row + col] = digit;
-		board.upFillCount();
+		board.validCellCount++;
 	};
 
 	this.removeDigit = function(row, col) {
-		board.grid[9 * row + col] = digit;
-		board.downFillCount();
+		if (board.grid[9 * row + col] !== 0) {
+			board.validCellCount--;
+		}
+		board.grid[9 * row + col] = 0;
 	};
 
 	this.checkInput = function(inputElement) {
@@ -77,23 +66,19 @@ function Board(boardContainer) {
 			$.merge(unionArray, cellBlock);
 
 			if (!isValidDigit(value) || ($.inArray(value, unionArray) !== -1)) {
-				board.showBadCell(inputElement);
+				inputElement.addClass('invalid');
 			} else {
 				board.insertDigit(row, col, value);
 				inputElement.removeClass('invalid');
 				if (board.validCellCount === 81) {
-					board.gameFinished(SOLVED);
+					board.gameFinished();
 				}
 			}
 		}
 	};
 
-	this.gameFinished = function(endState) {
+	this.gameFinished = function() {
 
-	};
-
-	this.showBadCell = function(inputElement) {
-		inputElement.addClass('invalid');
 	};
 
 	this.fetchRowDigits = function(row) {
